fd783c57bbb7b378f4f64a2fdc6269963b90a53b
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2021-05-25 00:49:34 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,337 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const fs = require('fs')
+const express = require('express')
+const api = express();
+const bodyParser = require("body-parser");
+const http = require('http')
+const https = require('https')
+const fetch = require('node-fetch')
+var shell = require('shelljs');
+const { clipboard } = require('electron')
+const  { networkInterfaces }  = require('os')
+const { dialog } = require('electron')
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+let config;
+
+if(fs.existsSync(path.join(__dirname, "config.json"))) {
+    config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")))
+} else {
+    config = {
+        "ip": "",
+        "intervall": 100
+    }
+}
+
+let raw = {}
+
+var ipInQueue = ""
+
+function SetupMulticast(localIP) {
+    var PORT = 5555;
+    var HOST = localIP;
+    var dgram = require('dgram');
+    var client = dgram.createSocket('udp4');
+
+    client.on('listening', function () {
+        var address = client.address();
+        console.log('UDP Client listening on ' + address.address + ":" + address.port);
+        client.setBroadcast(true)
+        client.setMulticastTTL(128); 
+        client.addMembership('225.1.1.1');
+    });
+
+    client.on('message', function (message, remote) {   
+        console.log('A: Epic Command Received. Preparing Relay.');
+        console.log('B: From: ' + remote.address + ':' + remote.port +' - ' + message);
+        ipInQueue = remote.address;
+        NotifyClient();
+    });
+
+    client.bind(PORT, HOST);
+}
+
+function NotifyClient() {
+    if(config.ip != ipInQueue) {
+        var answer = dialog.showMessageBoxSync(mainWindow, {
+            "message": "Incoming IP. Do you want to set this as your Quests IP? IP: " + ipInQueue,
+            "buttons": ["Yes", "No"],
+            "title": "Streamer tools client",
+            "type": "question"
+        })
+        if(answer == 0) {
+            config.ip = ipInQueue;
+            dialog.showMessageBoxSync(mainWindow, {
+                "message": "IP set to " + config.ip,
+                "buttons": ["OK"],
+                "title": "Streamer tools client"
+            })
+            mainWindow.reload();
+        } else {
+            console.log("IP not changed")
+        }
+    }
+}
+
+function GetLocalIPs() {
+    const nets = networkInterfaces();
+    const results = [];
+
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            if (net.family === 'IPv4' && !net.internal) {
+                results.push(net.address);
+                console.log("adding " + net.address)
+            }
+        }
+    }
+    return results
+}
+
+GetLocalIPs().forEach(ip => {
+    SetupMulticast(ip)
+})
+
+
+setInterval(() => {
+    fetch("http://" + config.ip + ":3501").then((res) => {
+        res.json().then((json) => {
+            raw = json
+            //console.log(json)
+        })
+    })
+}, config.intervall);
+
+
+
+UpdateOverlays().then(() => {
+    if(config.overlays.length != undefined) {
+        CheckOverlaysDownloaded();
+    }
+});
+
+function CheckOverlaysDownloaded() {
+    for(let i = 0; i < config.overlays.length; i++) {
+        var dir = path.join(__dirname, "overlays", config.overlays[i].Name)
+        if(fs.existsSync(dir)) {
+            config.overlays[i].downloaded = true;
+        } else {
+            config.overlays[i].downloaded = false;
+        }
+    }
+}
+
+function UpdateOverlays() {
+    return new Promise((resolve, reject) => {
+        fetch("https://computerelite.github.io/tools/Streamer_Tools_Quest_Overlay/overlays.json").then((res) => {
+            res.json().then((json) => {
+                var configBackup = config;
+                config.overlays = json.overlays;
+                configBackup.overlays.forEach(overlay => {
+                    var exists = false;
+                    config.overlays.forEach(item => {
+                        if(item.Name == overlay.Name)
+                        {
+                            exists = true;
+                            return;
+                        }
+                    })
+                    if(!exists) config.overlays.push(overlay)
+                })
+                saveConfig();
+                resolve();
+            })
+        })
+    })
+    
+}
+
+function saveConfig() {
+    writeToFile(path.join(__dirname, "config.json"), JSON.stringify(config))
+}
+
+function writeToFile(file, contents) {
+    fs.writeFile(file, contents, err => {
+        console.log(err)
+    })
+}
+
+
+function downloadFile(url, dest) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest, { flags: "wx" });
+
+        const request = https.get(url, response => {
+            if (response.statusCode === 200) {
+                response.pipe(file);
+            } else {
+                file.close();
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
+            }
+        });
+
+        request.on("error", err => {
+            file.close();
+            fs.unlink(dest, () => {}); // Delete temp file
+            reject(err.message);
+        });
+
+        file.on("finish", () => {
+            resolve();
+        });
+
+        file.on("error", err => {
+            file.close();
+
+            if (err.code === "EEXIST") {
+                reject("File already exists");
+            } else {
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(err.message);
+            }
+        });
+    });
+}
+
+function downloadOverlay(overlay) {
+    console.log("Downloading " + overlay.Name)
+    var dir = path.join(__dirname, "overlays", overlay.Name)
+    if(fs.existsSync(dir)) {
+        fs.rmdirSync(dir, {recursive: true}, err => {
+            console.log("error while deleting existing dir: " + err)
+        })
+    }
+    shell.mkdir(dir)
+    overlay.downloads.forEach(async function(download)  {
+        const fdir = path.join(dir, download.Path.substring(0, download.Path.lastIndexOf('/')))
+        if(!fs.existsSync(fdir)) {
+            shell.mkdir('-p', fdir);
+        }
+        downloadFile(download.URL, path.join(dir, download.Path))
+    });
+    CheckOverlaysDownloaded();
+}
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "html", "index.html"),
+        protocol: 'file',
+        slashes: true
+    }))
+})
+
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(bodyParser.json());
+api.use(bodyParser.raw());
+
+api.post(`/api/download`, async function(req, res) {
+    console.log(req.body.Name)
+    config.overlays.forEach(overlay => {
+        if(overlay.Name == req.body.Name) {
+            downloadOverlay(overlay);
+        }
+    })
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "html", "downloads.html"),
+        protocol: 'file',
+        slashes: true
+    }))
+})
+
+api.post(`/api/postip`, async function(req, res) {
+    config.ip = req.body.ip
+    saveConfig()
+    console.log("ip set to: " + config.ip)
+
+})
+
+api.post(`/api/copytoclipboard`, async function(req, res) {
+    clipboard.writeText(req.body.text)
+    console.log("wrote " + req.body.text + " to clipboard")
+})
+
+api.post(`/api/postintervall`, async function(req, res) {
+    config.intervall = req.body.intervall
+    saveConfig()
+    console.log("intervall set to: " + config.intervall)
+})
+
+api.get(`/api/getip`, async function(req, res) {
+    res.json({"ip": config.ip})
+})
+api.get(`/api/getintervall`, async function(req, res) {
+    res.json({"intervall": config.intervall})
+})
+
+api.get(`/api/getOverlay`, async function(req, res) {
+    var Url = new URL("http://localhost:501" + req.url)
+    var name = Url.searchParams.get("name")
+    var success = false;
+    config.overlays.forEach(overlay => {
+        if(overlay.Name == name && overlay.downloaded) {
+            overlay.downloads.forEach(download => {
+                if(download.IsEntryPoint) {
+                    res.redirect(url.format({
+                        pathname:"/overlays/" + overlay.Name + "/" + download.Path,
+                        query: req.query,
+                      }));
+                    success = true
+                    return;
+                }
+            })
+        }
+        if(success) return
+    })
+    if(!success) res.json({"msg": "error"})
+})
+
+api.get(`/windows/home`, async function(req, res) {
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "html", "index.html"),
+        protocol: 'file',
+        slashes: true
+    }))
+})
+
+api.get(`/windows/overlays`, async function(req, res) {
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "html", "overlays.html"),
+        protocol: 'file',
+        slashes: true
+    }))
+})
+
+api.get(`/windows/downloads`, async function(req, res) {
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "html", "downloads.html"),
+        protocol: 'file',
+        slashes: true
+    }))
+})
+
+api.get(`/api/overlays`, async function(req, res) {
+    res.json(config.overlays)
+})
+
+api.get(`/api/raw`, async function(req, res) {
+    var Url = new URL("http://localhost:501" + req.url)
+    var ip = Url.searchParams.get("ip")
+    if(ip != null && ip != "") {
+        config.ip = ip;
+    }
+    res.header("Access-Control-Allow-Origin", "*")
+    res.json(raw)
+})
+
+api.use("/overlays", express.static(path.join(__dirname, "overlays")))
+
+api.listen(501)
